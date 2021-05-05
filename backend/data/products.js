17fc6c20ac1d7e346cd9d5764b65f673310fc6ba
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2021-05-05 18:02:48 +0930

    fixing errors

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'NT Bus Tracker',
+    image: '/images/airpods.jpg',
+    description:
+      'A more visual interface. Real time location on screen Estimated time and the best route for your destination, Auto-mated text message service when your selected bus is nearby. A preferred route saving option',
+    brand: 'Individual',
+    category: 'Coding-Proficient',
+    price: 'People of all ages that uses the bus services or want to know about the transportation system of Darwin',
+    countInStock: 3,
+    rating: 0,
+    numReviews: 4,
+  },
+  {
+    name: 'Money Management',
+    image: '/images/phone.jpg',
+    description:
+      'This app will track income and expenses and provide relevant information',
+    brand: 'Team',
+    category: 'Coding-Capable',
+    price: 'Working people between 18-40 years old',
+    countInStock: 10,
+    rating: 0,
+    numReviews: 4,
+  },
+  {
+    name: 'Time management app',
+    image: '/images/camera.jpg',
+    description:
+      'Helps to manage schedule as the users can see graph of time division and it helps to know the live location of the user',
+    brand: 'Individual',
+    category: 'Coding-Development',
+    price: 'Everyone',
+    countInStock: 5,
+    rating: 0,
+    numReviews: 3,
+  },
+  {
+    name: 'NT Fuel and Services',
+    image: '/images/playstation.jpg',
+    description:
+      'Store fuel purchase information, analyze your fuel expenses, find local services and navigate a fuel map',
+    brand: 'Individual',
+    category: 'Coding-Proficient',
+    price: 'Ages 18-40 and expense wise people',
+    countInStock: 10,
+    rating: 0,
+    numReviews: 3,
+  },
+  {
+    name: 'Pocket Library Alpha',
+    image: '/images/mouse.jpg',
+    description:
+      'Pocket Library demnstrates an internal structure of CDU library (Casuarina campus) as well as major functions adn indicators',
+    brand: 'Team',
+    category: 'Coding-Development',
+    price: 'Students and academics',
+    countInStock: 7,
+    rating: 0,
+    numReviews: 2,
+  },
+  {
+    name: 'Simple Memo Phone App',
+    image: '/images/alexa.jpg',
+    description:
+      'A web-based memo app designede for simplicity and functionality',
+    brand: 'Individual',
+    category: 'Coding-Capable',
+    price: 'Forgetful people such as students, elderly patients on scheduled medicine, people with busy schedules and people who leads busy lives',
+    countInStock: 0,
+    rating: 0,
+    numReviews: 4,
+  },
+]
+
+export default products
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To Judge Website',
+  description: 'This website is used to Judge all the projects in CDU',
+  keywords: 'judge, projects, CDU',
+}
+
+export default Meta
